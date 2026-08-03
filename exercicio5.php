dcda0b4877e62ec4ea55a42ae39929f57182207f
gabrields160326-pixel <gabrields160326@gmail.com>
2026-08-03 16:55:19 -0300

    exercicio 5 proz

---
+++ exercicio5.php
@@ -0,0 +1,17 @@
+<?php
+
+$nota1;
+$nota2;
+$nota3;
+$media;
+
+$nota1 = readline("Digite a primeira nota: ");
+$nota2 = readline("Digite a segunda nota: ");
+$nota3 = readline("Digite a terceira nota: ");
+
+$soma = $nota1 + $nota2 + $nota3;
+$media = $soma / 3;
+
+print("A media das suas notas é: $media");
+
+?>
