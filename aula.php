b77b7640b181f265e945163014cb79752b74305a
gabrields160326-pixel <gabrields160326@gmail.com>
2026-08-03 16:49:30 -0300

    Add PHP script to sum two numbers

---
+++ aula.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1 = readline("Dig
+ite o primeiro numero: ");
+
+$num2 = readline("Digite o segundo numero: ");
+
+$resultado;
+
+$resultado = $num1 + $num2;
+
+print("A entre o $num1 e $num2 é $resultado");
+
+?>
