b5ca0bead6f713c5ae00ef5e4b4feec061f792b8
gabrields160326-pixel <gabrields160326@gmail.com>
2026-08-31 15:56:42 -0300

    Update exercicio1poo.php

--- exercicio1poo.php
+++ exercicio1poo.php
@@ -1,39 +1,43 @@
 <?php
-class Produto {
+	class Produto{
+		public string $nome;
+		public float $preco;
+		public int $quantidade;
 
-	public string $nome;
-	public float $preco;
-	public int $quantidade;
-
-	public function __construct(string $nome, float $preco, int $quantidade) {
-		$this->nome = $nome;
-		$this->preco = $preco;
-		$this->quantidade = $quantidade;
+		public function calcularTotal(){
+			return $this->preco * $this->quantidade;
+		}
 	}
 
-	public function calcularTotal(): float {
-		return $this->preco * $this->quantidade;
-	}
-}
-
-$produto1 = new Produto("Iphone", 3500.10, 5);
-$produto2 = new Produto("PC Gamer", 6750.50, 3);
-
-echo "Produto: " . $produto1->nome . "\n";
-echo "Preço: " . $produto1->preco . "\n";
-echo "Quantidade: " . $produto1->quantidade . "\n";
-echo "Valor total: " . $produto1->calcularTotal() . "\n";
-
-echo "----------------------------------------\n";
-
-echo "Produto: " . $produto2->nome . "\n";
-echo "Preço: " . $produto2->preco . "\n";
-echo "Quantidade: " . $produto2->quantidade . "\n";
-echo "Valor total: " . $produto2->calcularTotal() . "\n";
+$produto1 = new Produto();
+$produto1->nome = "PC";
+$produto1->preco = 5000;
+$produto1->quantidade = 3;
 
 
-$totalGeral = $produto1->calcularTotal() + $produto2->calcularTotal();
-echo "Valor total dos dois produtos: " . $totalGeral . "\n";
+echo "Produto 1: " . $produto1->nome;
+echo "\n";
+echo "Preço: " . $produto1->preco;
+echo "\n";
+echo "Quantidade: " . $produto1->quantidade;
+echo "\n";
+echo "Total: " . $produto1->calcularTotal();
+
+echo "\n";
+echo "-------------------\n";
+
+$produto2 = new Produto();
+$produto2->nome = "Iphone";
+$produto2->preco = 5000;
+$produto2->quantidade = 5;
+
+echo "Produto 2: " . $produto2->nome;
+echo "\n";
+echo "Preço: " . $produto2->preco;
+echo "\n";
+echo "Quantidade: " . $produto2->quantidade;
+echo "\n";
+echo "Total: " . $produto2->calcularTotal();
 
 
 ?>
