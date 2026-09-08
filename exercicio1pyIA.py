734d1644306f634a53c4017a0b76fa2f117012d5
gabrields160326-pixel <gabrields160326@gmail.com>
2026-09-08 14:36:24 -0300

    Create exercicio1pyIA.py

---
+++ exercicio1pyIA.py
@@ -0,0 +1,12 @@
+num1 = 9
+num2 = 10
+
+soma = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print(f"Numero 1:  {soma}")
+print(f"Numero 2: {sub}")
+print(f"Numero 3: {mult}")
+print(f"Numero 4: {div:.2f}")
