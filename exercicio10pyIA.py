531209e8a9b46a9dd96733342ccdc3dd0413afac
gabrields160326-pixel <gabrields160326@gmail.com>
2026-09-10 14:45:56 -0300

    Create exercicio10pyIA.py

---
+++ exercicio10pyIA.py
@@ -0,0 +1,9 @@
+def calcular_area_retangulo(base, altura):
+    area = base * altura
+    return area
+
+base = float(input("Digite a base do retangulo: "))
+altura = float(input("Digite a altura do retangulo: "))
+
+
+print(calcular_area_retangulo(base, altura))
