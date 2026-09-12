379183663c6557e02894b7e6f42b9f0bb2c79974
gabrields160326-pixel <gabrields160326@gmail.com>
2026-09-12 10:46:15 -0300

    Update exercicio6pyIA.py

--- exercicio6pyIA.py
+++ exercicio6pyIA.py
@@ -1,6 +1,11 @@
-senha = input("Digite a senha: ")
+senha = ""
+tentativas = 0
+
+while senha != "python123" and tentativas < 3:
+    senha = input("Digite sua senha: ")
+    tentativas += 1
 
 if senha == "python123":
     print("Pode entrar")
 else:
-    print("Nao pode entrar")
+    print("broquiadu")
