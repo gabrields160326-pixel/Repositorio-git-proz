df1e83cba16d737045165b5dda6f59365d1c4d97
gabrields160326-pixel <gabrields160326@gmail.com>
2026-09-12 10:14:37 -0300

    Create exercicio3pyIA(Ref).py

---
+++ exercicio3pyIA(Ref).py
@@ -0,0 +1,12 @@
+nota1 = 5
+nota2 = 7
+nota3 = 9
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
